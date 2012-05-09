30c3e11f9e884ff76a1819cda7054abe84d279cf
Christiaan Baes <chris.baes@skynet.be>
2012-05-09 10:02:16 +0200

    Made solution and added insert and update projects

---
+++ 2.UpdateData/Module1.vb
@@ -0,0 +1,35 @@
+﻿Option Strict Off
+
+Module Module1
+
+    Sub Main()
+        Dim db = SetupDatabase.SqlCompactDatabase.Setup(True)
+        ' Showing data in address table with id = 1
+        Dim address = db.Address.FindById(1)
+        Console.WriteLine("{0} {1} {2}", address.Id, address.Street, address.HouseNumber)
+        ' Showing data in address table with id = 2
+        address = db.Address.FindById(2)
+        Console.WriteLine("{0} {1} {2}", address.Id, address.Street, address.HouseNumber)
+        ' Showing data in persons table with id = 1
+        Dim person = db.Person.FindById(1)
+        Console.WriteLine("{0} {1} {2}", person.Id, person.LastName, person.FirstName)
+        ' Updating data in Address table using anonymous object using ById
+        db.Address.UpdateById(New With {.Id = 1, .Street = "street1updated", .HouseNumber = "1updated"})
+        ' Updating data in Address table using named parameters
+        db.Address.UpdateById(Id:=2, Street:="street2updated", HouseNumber:="2updated")
+        ' Updating data in Persons table using anonymous object using primary key
+        db.Person.Update(New With {.Id = 1, .LastName = "lastname1updated", .FirstName = "firstname1updated", .AddressId = 1})
+        ' Showing data in address table with id = 1
+        address = db.Address.FindById(1)
+        Console.WriteLine("{0} {1} {2}", address.Id, address.Street, address.HouseNumber)
+        ' Showing data in address table with = 1
+        address = db.Address.FindById(2)
+        Console.WriteLine("{0} {1} {2}", address.Id, address.Street, address.HouseNumber)
+        ' Showing data in persons table with id = 1
+        person = db.Person.FindById(1)
+        Console.WriteLine("{0} {1} {2}", person.Id, person.LastName, person.FirstName)
+        ' Waiting for input
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ SetupDatabase/SQLCompactDatabase.vb
@@ -0,0 +1,70 @@
+﻿Option Strict Off
+
+Imports System.Data.SqlServerCe
+Imports System.IO
+Imports Simple.Data
+
+Public Module SqlCompactDatabase
+
+    Function Setup(withTestData As Boolean) As Object
+        CreateDatabase()
+        CreateTables()
+        Dim db = Database.OpenConnection(ConnectionString)
+        If withTestData Then InsertTestData(db)
+        Return db
+    End Function
+
+    Private Sub InsertTestData(ByVal db As Object)
+        db.Address.Insert(New With {.Street = "street1", .HouseNumber = "1"})
+        db.Address.Insert(New With {.Street = "street2", .HouseNumber = "2"})
+        db.Person.Insert(New With {.LastName = "lastname1", .FirstName = "firstname1", .AddressId = 1})
+        db.Person.Insert(New With {.LastName = "lastname1", .FirstName = "firstname2", .AddressId = 1})
+        db.Person.Insert(New With {.LastName = "lastname2", .FirstName = "firstname3", .AddressId = 2})
+        db.BadHabit.Insert(New With {.BadHabit = "Drinks"})
+        db.BadHabit.Insert(New With {.BadHabit = "Smokes"})
+        db.BadHabit.Insert(New With {.BadHabit = "Eats to much"})
+        db.BadHabitPerson.Insert(New With {.BadHabitId = 1, .PersonId = 1})
+        db.BadHabitPerson.Insert(New With {.BadHabitId = 2, .PersonId = 1})
+        db.BadHabitPerson.Insert(New With {.BadHabitId = 3, .PersonId = 2})
+        db.BadHabitPerson.Insert(New With {.BadHabitId = 3, .PersonId = 1})
+        db.BadHabitPerson.Insert(New With {.BadHabitId = 3, .PersonId = 3})
+    End Sub
+
+    Public Function ConnectionString() As String
+        Return "DataSource=""test.sdf""; Password=""mypassword"""
+    End Function
+
+    Private Function CreateDatabase() As SqlCeEngine
+        If File.Exists("test.sdf") Then File.Delete("test.sdf")
+        Dim en = New SqlCeEngine(ConnectionString)
+        en.CreateDatabase()
+        Return en
+    End Function
+
+    Private Sub CreateTables()
+        Using cn = New SqlCeConnection(ConnectionString)
+            If cn.State = ConnectionState.Closed Then
+                cn.Open()
+            End If
+            For Each SqlScript In CreateTableScripts()
+                Using cmd = New SqlCeCommand(SqlScript, cn)
+                    cmd.ExecuteNonQuery()
+                End Using
+            Next
+        End Using
+    End Sub
+
+    Private Function CreateTableScripts() As IList(Of String)
+        Dim s As New List(Of String)
+        s.Add("CREATE TABLE Address (Id int IDENTITY(1,1) PRIMARY KEY, Street nvarchar (40) NOT NULL, HouseNumber nvarchar (10))")
+        s.Add("CREATE TABLE Person (Id int IDENTITY(1,1) PRIMARY KEY, LastName nvarchar (40) NOT NULL, FirstName nvarchar (40), AddressId int NOT NULL)")
+        s.Add("ALTER TABLE Person ADD CONSTRAINT FK_Person_Address FOREIGN KEY (AddressId) REFERENCES Address(Id)")
+        s.Add("CREATE TABLE BadHabit (Id int IDENTITY(1,1) PRIMARY KEY, BadHabit nvarchar (40) NOT NULL)")
+        s.Add("CREATE TABLE BadHabitPerson (BadHabitId int NOT NULL, PersonId int NOT NULL)")
+        s.Add("ALTER TABLE BadHabitPerson ADD PRIMARY KEY(BadHabitId, PersonId)")
+        s.Add("ALTER TABLE BadHabitPerson ADD CONSTRAINT FK_BadHabitPerson_Person FOREIGN KEY (PersonId) REFERENCES Person(Id)")
+        s.Add("ALTER TABLE BadHabitPerson ADD CONSTRAINT FK_BadHabitPerson_BadHabit FOREIGN KEY (BadHabitId) REFERENCES BadHabit(Id)")
+        Return s
+    End Function
+
+End Module
